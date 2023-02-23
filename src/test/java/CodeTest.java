4a86b50a2d097cbef2e809a429ff35f3091824af
Yuri <yuri.barru@gmail.com>
2023-02-24 06:55:06 +0700

    Initial commit

---
+++ src/test/java/CodeTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CodeTest {
+
+    @Test
+    public void testSayHello(){
+        //create new instance of class Code
+        Code code = new Code();
+
+        assertEquals("Hello world", code.sayHello());
+    }
+}
