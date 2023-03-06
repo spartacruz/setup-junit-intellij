c5c67f7de3d0b5ea723329efd1110355f6d12fa2
Yuri <yuri.barru@gmail.com>
2023-03-06 14:50:46 +0700

    learning custom message and report topic

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -23,7 +23,7 @@ public class BankAccountTest {
         //new instance
         BankAccount bankAccount = new BankAccount(7000, 100);
         bankAccount.deposit(5000);
-        assertEquals(12000, bankAccount.getBalance());
+        assertEquals(12000, bankAccount.getBalance(), "Unexpected value, should be 12k");
     }
     @Test
     @DisplayName("Withdraw will become negative")
