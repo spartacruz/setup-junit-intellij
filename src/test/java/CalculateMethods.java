5ab1528108f30630693235a1992414baab2ae69f
Yuri <yuri.barru@gmail.com>
2023-03-08 14:43:50 +0700

    Learn to mock with Mockito

---
+++ src/test/java/CalculateMethods.java
@@ -0,0 +1,13 @@
+public class CalculateMethods {
+
+    private int x;
+    private int y;
+    public CalculateMethods(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double divide(int x, int y) {
+        return x * y;
+    }
+}
